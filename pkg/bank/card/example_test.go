7c426d2c842c7f5d50f23ced63790f1fcc154efa
Larisa Kasyanova <lkasyanova@velox.biz>
2021-03-25 11:27:09 +0500

    fixed "lesson 9_1"

--- pkg/bank/card/example_test.go
+++ pkg/bank/card/example_test.go
@@ -92,3 +92,36 @@ func ExampleTotal() {
 	fmt.Println(Total(cards))
 	//Output:2000000
 }
+func ExamplePaymentSources() {
+	cards := []types.Card{
+		{
+			Balance: 1_000_00,
+			Active:  true,
+			PAN:     "salom",
+		},
+		{
+			Balance: 2_000_00,
+			Active:  true,
+			PAN:     "depozit",
+		},
+		{
+			Balance: 1_000_00,
+			Active:  false,
+			PAN:     "alek",
+		},
+		{
+			Balance: -1_000_00,
+			Active:  true,
+			PAN:     "qwerty",
+		},
+	}
+	r := PaymentSources(cards)
+	for _, source := range r {
+		fmt.Println(source.Number)
+	}
+	//Output:
+	//salom
+	//depozit
+	//
+	//
+}
--- pkg/bank/types/types.go
+++ pkg/bank/types/types.go
@@ -1,6 +1,5 @@
 package types
 
-
 type Currency string
 
 const (
@@ -13,17 +12,24 @@ type Money int64
 type PAN string
 
 type Card struct {
-Id       int
-PAN      PAN
-Balance  Money
-Currency Currency
-Color    string
-Name     string
-Active   bool
-MinBalance Money
+	Id         int
+	PAN        PAN
+	Balance    Money
+	Currency   Currency
+	Color      string
+	Name       string
+	Active     bool
+	MinBalance Money
 }
+
 // Payment provides information about the payment
-type Payment struct{
-	ID int
+type Payment struct {
+	ID     int
 	Amount Money
+}
+
+type PaymentSource struct {
+	Type    string
+	Number  string
+	Balance Money
 }
